c80da21c17d193bd6fcc3b7b10264012927a0e43
Rachel DiCesare <racheldicesare@Rachels-iMac.attlocal.net>
2020-10-29 10:34:24 -0500

    downloaded sass and added some styles for my counter.scss

--- counter/src/counter/counter.js
+++ counter/src/counter/counter.js
@@ -1,42 +1,48 @@
 import React, { Component } from "react";
+import "./counter.scss";
 
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
     };
-}
+  }
 
-
-increment = () => {
+  increment = () => {
     this.setState({
-       count: this.state.count + 1
+      count: this.state.count + 1,
     });
-}
+  };
 
-decrement = () => {
+  decrement = () => {
     this.setState({
-       count: this.state.count - 1
+      count: this.state.count - 1,
     });
-}
+  };
 
-reset = () => {
+  reset = () => {
     this.setState({
-        count: 0
-    })
-}
-
-
+      count: 0,
+    });
+  };
 
   render() {
     return (
-    <div className="counter">
-     <span>{this.state.count}</span>
-     <button className="inc" onClick={this.increment}>+</button>
-     <button className="dec" onClick={this.decrement}>-</button>
-     <button className="reset" onClick={this.reset}>Reset</button>
-    </div>
-    )
+      <div className="counter">
+        <span>{this.state.count}</span>
+        <div className="buttons">
+          <button className="inc" onClick={this.increment}>
+            +
+          </button>
+          <button className="dec" onClick={this.decrement}>
+            -
+          </button>
+          <button className="reset" onClick={this.reset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    );
   }
 }
